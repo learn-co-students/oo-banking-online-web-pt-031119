edb5ea36545cbe9edb79535ed22276d911197559
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-10 22:42:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  
+  attr_accessor  :balance, :status, :new_balance
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open"
+  end 
+
+  def deposit(total)
+    @balance = @balance + total 
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
+  
+  def valid?
+    if @balance > 0 && @status == "open"
+      true 
+    else 
+      false 
+    end 
+  end 
+  
+  def close_account 
+    @status = "closed"
+  end 
 
 end
