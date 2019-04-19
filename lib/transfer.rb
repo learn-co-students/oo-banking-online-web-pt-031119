06a525ee71107950d334f97eadaa758441e0fc11
Samuel T <ssamt1142@gmail.com>
2019-04-19 02:05:12 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :status, :sender, :receiver, :amount
+attr_reader :name
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender = BankAccount.new(name)
+    receiver = BankAccount.new(name)
+    sender.valid?
+    receiver.valid?
+  end
+
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
 end
