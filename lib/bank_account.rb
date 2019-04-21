57687eec06590cecc7611a61e7c92ae2faed395c
Patrick A <akolo@hotmail.com>
2019-04-21 12:52:31 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
-
+  
+  attr_accessor :name, :balance,
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+  end 
+ def status
+   status = "open"
+ end 
+ 
+ def deposit
+   @balance 
+ end
+ 
+ def display_balance 
+   @balance 
+   
+ end
+ 
+ def valid?
+   
+ end 
+ 
+ def 
 end
